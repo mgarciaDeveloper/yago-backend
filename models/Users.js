0dd4c234b98bf9411103c88f9ba6037c89288d0e
Matheus Garcia <mgarcia.developer@gmail.com>
2022-07-08 17:28:41 -0300

    username e senha

--- models/Users.js
+++ models/Users.js
@@ -6,13 +6,12 @@ const encrypt = require("mongoose-encryption");
 //vamos criar um schema de collection, chamada 'ProductSchema'
 const UserSchema = new mongoose.Schema({
   nome: { type: String, required: [true, "Este campo é obrigatório"] },
-  username: { type: Number, required: [true, "Este campo é obrigatório"] },
+  username: { type: String, required: [true, "Este campo é obrigatório"] },
   password: { type: String, required: [true, "Este campo é obrigatório"] },
   cidade: { type: String, required: [true, "Este campo é obrigatório"] },
   telefone: { type: String, required: [true, "Este campo é obrigatório"] },
 });
 
-//vamos criar uma collection. Essa collection vai ter o formato da 'ProductSchema'
 UserSchema.plugin(passportLocalMongoose); //criptografia de password e um esquema de login com username & password
 
 UserSchema.plugin(encrypt, {
